6eb39de80d96b6af332004b4e47d87d7d9b54e3a
Nik Rowe <NikRowe7@Gmail.com>
2020-06-03 11:19:03 -0700

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // We import the mongodb obj from MongoDB //
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert').strict
+const dboper = require('./operations')
 
 // url in which to access the mongo server //
 const url = 'mongodb://localhost:27017'
@@ -22,24 +23,34 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
         assert.strictEqual(err, null)
         console.log('Dropped Collection', result)
 
-        // re-create the campsite collection // 
-        const collection = db.collection('campsites')
-
         // insert a new document into the campsites collection // 
-        collection.insertOne({name: "Breadcrumb Trail Campground", description: "Test"},
-        (err, result) => {
-            assert.strictEqual(err, null)
+        dboper.insertDocument(db, {name: "Breadcrumb Trail Campground", description: "Test"},
+            'campsites', result => {
             console.log('Insert Document: ', result.ops)
-
-            // find() to console log all the documents inside the campsites collection // 
-            collection.find().toArray((err, docs) => {
-                assert.strictEqual(err, null)
+            
+            dboper.findDocuments(db, 'campsites', docs => {
                 console.log('Found Documents:', docs)
 
-                client.close()
+                dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" }, 
+                    { description: "Updated Test Description"}, 'campsites', 
+                    result => {
+                        console.log('Updated Document Count:', result.result.nModified)
+
+                        dboper.findDocuments(db, 'campsites', docs => {
+                            console.log('Found Documents:', docs)
+
+                            dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
+                                'campsites', result => {
+                                    console.log('Deleted Document Count:', result.deletedCount)
+                             
+                                    client.close()
+                                }
+                            )
+                        })
+                    }
+                )
             })
         })
     })
-
 })
 
